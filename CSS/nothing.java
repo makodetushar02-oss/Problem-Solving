7fe167d3bfe7b7a7b2c30899dd10eff2044eae61
Tushar Makode <makodetushar02@gmail.com>
2026-06-20 20:16:13 +0530

    new test

---
+++ CSS/nothing.java
@@ -0,0 +1,10 @@
+
+public class nothing {
+    public static void main(String[] args) {
+        int A = 20;
+        int B = A-- ;
+        System.out.println(A);
+        System.out.println(B);
+    }
+
+}
