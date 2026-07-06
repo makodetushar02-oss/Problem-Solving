70ba80ab5e0815cedad7efb83c1f114a70adf691
Tushar Makode <makodetushar02@gmail.com>
2026-07-06 22:28:45 +0530

    just commit

--- java/LeapYear.java
+++ java/LeapYear.java
@@ -1,18 +1,17 @@
-import java.util.Scanner;
+import java.util.*;
+public class LeapYear {
 
-public class LeapYear{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int Y = sc.nextInt();
-
-        if( Y % 100 == 0){
-            System.out.println("not a leap year");
-        } 
-        else if(Y % 4 == 0){
-            System.out.println("leap year");
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter Year to be check leap year or not");
+        int N = sc.nextInt();
+        if(N%400 == 0 ){
+            System.out.println(" leap year");
         }
-        else{
+        else if(N%4 != 0 || N%100 == 0){
             System.out.println("not a leap year");
+        }else {
+            System.out.println(" leap year");
         }
         sc.close();
     }
